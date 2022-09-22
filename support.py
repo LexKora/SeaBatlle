33251feadbe74c7cec34c58a232bdfdce03f1370
LexKora <kargin.alexej2010@yandex.ru>
2022-09-22 17:14:24 +0300

    1.2
    почти всё кроме реализации случая когда пользователь загнал расстановку в тупик

--- support.py
+++ support.py
@@ -32,7 +32,6 @@ def checking_or_ships_nearby(Field, cord, human, coordinates):
     num = None
     if num == None:
         num = convert_number_to_digits(cord)#введённые координаты
-    print(Field.LINES[num[0]-1][num[1]-1][0])
 
     if Field.LINES[num[0]-1][num[1]-1][0] == Field.SHIP:  # если ставим на занятую клетку
         print(E + ' Эта позиция уже занята!')
@@ -55,6 +54,24 @@ def checking_or_ships_nearby(Field, cord, human, coordinates):
             else:
                 prev = convert_number_to_digits(coordinates[-1])
                 print(f'{num[0] - prev[0]}   +    {num[1] - prev[1]}')
+                if len(coordinates) == 2:  # Что бы не был углом трёхпалубный
+                    print(coordinates)
+                    a = convert_number_to_digits(coordinates[0])
+                    b = convert_number_to_digits(coordinates[1])
+                    print(f'{a[0]}{min(a[1], b[1]) - 1}    {a[0]}{max(a[1], b[1]) + 1} num = {num}')
+                    if a[0] == b[0]:
+                        if cord == convert_digits_to_number(a[0], min(a[1], b[1])-1) or\
+                            cord == convert_digits_to_number(a[0], max(a[1], b[1])+1):
+                            print(f'{a[0]}{min(a[1], b[1])-1}    {a[0]}{max(a[1], b[1])+1} num = {num}')
+                            return True
+                    if a[1] == b[1]:
+                        if cord == convert_digits_to_number(min(a[0], b[0])-1, a[1]) or\
+                            cord == convert_digits_to_number(max(a[0], b[0])+1, a[1]):
+                            return True
+                    else:
+                        print(E +' Трёхпалубный корабль должен быть расположен по прямой и без пробелов!')
+                        return False
+
                 if abs(num[0] - prev[0]) > 1 or abs(num[1] - prev[1]) > 1:
                     print(E + ' Не должно быть пробелов между палубами корабля!')
                     return False
@@ -64,7 +81,8 @@ def checking_or_ships_nearby(Field, cord, human, coordinates):
                 if cordindefsym == Field.SHIP and coordinates[-1] == str(num[0] + i - 1) + str(num[1] + j - 1):
                     print(f'{coordinates[-1]} -- {str(num[0] + i - 1) + str(num[1] + j - 1)}')
                     return next_to_another_ship()# если координаты совпадают с предидущей палубой
-                if len(coordinates) == 2:
+
+
 
     print('True')
     return True
@@ -74,3 +92,7 @@ def convert_number_to_digits(num):
     character1 = int(list(str(num))[0])
     character2 = int(list(str(num))[1])
     return character1, character2
+def convert_digits_to_number(a, b):
+    number = str(a) + str(b)
+    return number
+
